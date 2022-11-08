1591bf45f9da562e9ba790d17a10dff93aa7b848
Ricardo <ricardo270298@gmail.com>
2022-11-08 12:58:15 -0400

    [FIX] Browser Router basename added

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -22,7 +22,7 @@ const App = () => {
     const initialState = useInitialState()
     return (
       <AppContext.Provider value={initialState}>
-        <BrowserRouter>
+        <BrowserRouter basename='/react-practico'>
           <Layout>
             <Routes>
               <Route exact path="/" element={<Home />} />
